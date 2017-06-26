984bd2400762a6684dce6700396345bd681abce8
Nick Loman <n.j.loman@bham.ac.uk>
2017-06-26 12:18:22 +0100

    ansible recipes

---
+++ texas/ansible/go.sh
@@ -0,0 +1,5 @@
+ansible-playbook assembly-stats.yaml -u nickloman
+ansible-playbook copy.yaml -u nickloman
+ansible-playbook python.yaml -u nickloman
+ansible-playbook rstudio.yaml -u nickloman
+ansible-playbook users.yaml -u nickloman
