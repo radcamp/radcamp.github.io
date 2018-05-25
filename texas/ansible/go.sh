d29b84b844be47095d7051df6c8f5a0a01f9d1ad
Isaac Overcast <isaac.overcast@gmail.com>
2018-05-25 15:13:03 -0400

    More cleanup

--- texas/ansible/go.sh
+++
@@ -1,5 +0,0 @@
-ansible-playbook assembly-stats.yaml -u nickloman
-ansible-playbook copy.yaml -u nickloman
-ansible-playbook python.yaml -u nickloman
-ansible-playbook rstudio.yaml -u nickloman
-ansible-playbook users.yaml -u nickloman
--- texas/ansible/install_assembly_stats.sh
+++
@@ -1,9 +0,0 @@
-cd ~
-git clone https://github.com/sanger-pathogens/assembly-stats.git
-cd assembly-stats
-mkdir build
-cd build
-cmake ..
-make
-make test
-make install
